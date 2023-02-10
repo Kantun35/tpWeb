a25939b9133946bd53b4e7cc87b767497b941948
Kantun35 <quentin.bigot35830@gmail.com>
2023-02-10 09:46:25 +0100

    début creation model

--- model.js
+++ model.js
@@ -1,3 +1,30 @@
-
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Drawing() {
+    this.formes = [];
+    
+    
+}
+
+function Forme() {
+    this.couleur;
+    this.epaisseur;
+    
+}
+
+function Rectangle() {
+    Forme.call();
+    this.coordHG_x = 0;
+    this.coordHG_y = 0;
+    this.largeur = 0;
+    this.hauteur = 0;
+}
+
+function Line() {
+    Forme.call();
+    this.coord1x = 0;
+    this.coord1y = 0;
+    this.coord2x = 0;
+    this.coord2y = 0;
+}
